f9889d7fa82c5ae243e5e9ff201f61b218439892
Cédric Belin <cedric@belin.io>
2016-11-08 23:42:21 +0100

    Added new empty unit tests

---
+++ test/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Implementation of the `akismet\test\ClientTest` class.
+ */
+namespace akismet\test;
+use akismet\{Client};
+
+/**
+ * Tests the features of the `akismet\Client` class.
+ */
+class ClientTest extends \PHPUnit_Framework_TestCase {
+
+}
---
+++ test/CommentTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Implementation of the `akismet\test\CommentTest` class.
+ */
+namespace akismet\test;
+use akismet\{Comment};
+
+/**
+ * Tests the features of the `akismet\Comment` class.
+ */
+class CommentTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests the `Comment` constructor.
+   */
+  public function testConstructor() {
+    // TODO
+  }
+
+  /**
+   * Tests the `Comment::fromJSON()` method.
+   */
+  public function testFromJSON() {
+    // TODO
+  }
+
+  /**
+   * Tests the `Comment::toJSON()` method.
+   */
+  public function testToJSON() {
+    // TODO
+  }
+}

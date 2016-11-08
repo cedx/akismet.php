890885825ff57e0491239c91a5fe31c428278ced
Cédric Belin <cedric@belin.io>
2016-11-08 22:41:29 +0100

    Added getter/setter methods

--- lib/Author.php
+++ lib/Author.php
@@ -12,40 +12,41 @@ class Author implements \JsonSerializable {
   /**
    * @var string The author's mail address.
    */
-  public $email = '';
+  private $email = '';
 
   /**
    * @var string The author's IP address.
    */
-  public $ipAddress = '';
+  private $ipAddress = '';
 
   /**
    * @var string The author's name.
    */
-  public $name = '';
+  private $name = '';
 
   /**
-   * @var string The role of the author. If you set it to `"administrator"`, Akismet will always return `false`.
+   * @var string The author's role.
    */
-  public $role = '';
+  private $role = '';
 
   /**
    * @var string The URL of the author's website.
    */
-  public $url = '';
+  private $url = '';
 
   /**
    * @var string The author's user agent, that is the string identifying the Web browser used to submit comments.
    */
-  public $userAgent = '';
+  private $userAgent = '';
 
   /**
    * Initializes a new instance of the class.
    * @param array $config Name-value pairs that will be used to initialize the object properties.
    */
-  public function __construct($config = []) {
+  public function __construct(array $config = []) {
     foreach ($config as $property => $value) {
-      if(property_exists($this, $property)) $this->$property = $value;
+      $setter = "set$property";
+      if(method_exists($this, $setter)) $this->$setter($value);
     }
   }
 
@@ -55,29 +56,133 @@ class Author implements \JsonSerializable {
    * @return Author The instance corresponding to the specified JSON map, or `null` if a parsing error occurred.
    */
   public static function fromJSON($map) {
-    return !is_array($map) || !$map ? null : new static([
+    return !is_array($map) ? null : new static([
       'email' => isset($map['comment_author_email']) && is_string($map['comment_author_email']) ? $map['comment_author_email'] : '',
-      'ipAddress' => isset($map['user_ip']) && is_string($map['comment_author_email']) ? $map['user_ip'] : '',
-      'name' => isset($map['comment_author']) && is_string($map['comment_author_email']) ? $map['comment_author'] : '',
-      'role' => isset($map['user_role']) && is_string($map['comment_author_email']) ? $map['user_role'] : '',
-      'url' => isset($map['comment_author_url']) && is_string($map['comment_author_email']) ? $map['comment_author_url'] : '',
-      'userAgent' => isset($map['user_agent']) && is_string($map['comment_author_email']) ? $map['user_agent'] : ''
+      'ipAddress' => isset($map['user_ip']) && is_string($map['user_ip']) ? $map['user_ip'] : '',
+      'name' => isset($map['comment_author']) && is_string($map['comment_author']) ? $map['comment_author'] : '',
+      'role' => isset($map['user_role']) && is_string($map['user_role']) ? $map['user_role'] : '',
+      'url' => isset($map['comment_author_url']) && is_string($map['comment_author_url']) ? $map['comment_author_url'] : '',
+      'userAgent' => isset($map['user_agent']) && is_string($map['user_agent']) ? $map['user_agent'] : ''
     ]);
+  }
+
+  /**
+   * Gets the author's mail address.
+   * @return string The author's mail address.
+   */
+  public function getEmail(): string {
+    return $this->email;
+  }
+
+  /**
+   * Gets the author's IP address.
+   * @return string The author's IP address.
+   */
+  public function getIpAddress(): string {
+    return $this->ipAddress;
+  }
+
+  /**
+   * Gets the author's name.
+   * @return string The author's name.
+   */
+  public function getName(): string {
+    return $this->name;
+  }
+
+  /**
+   * Gets the author's role.
+   * @return string The author's role.
+   */
+  public function getRole(): string {
+    return $this->role;
+  }
+
+  /**
+   * Gets the URL of the author's website.
+   * @return string The URL of the author's website.
+   */
+  public function getURL(): string {
+    return $this->url;
+  }
+
+  /**
+   * Gets the author's user agent, that is the string identifying the Web browser used to submit comments.
+   * @return string The author's user agent.
+   */
+  public function getUserAgent(): string {
+    return $this->userAgent;
   }
 
   /**
    * Converts this object to a map in JSON format.
    * @return array The map in JSON format corresponding to this object.
    */
-  public function jsonSerialize(): array {
+  final public function jsonSerialize(): array {
+    return $this->toJSON();
+  }
+
+  /**
+   * Sets the author's mail address.
+   * @param string $value The new mail address.
+   */
+  public function setEmail(string $value) {
+    $this->email = $value;
+  }
+
+  /**
+   * Sets the the author's IP address.
+   * @param string $value The new IP address.
+   */
+  public function setIpAddress(string $value) {
+    $this->ipAddress = $value;
+  }
+
+  /**
+   * Sets the author's name.
+   * @param string $value The new name.
+   */
+  public function setName(string $value) {
+    $this->name = $value;
+  }
+
+  /**
+   * Sets the author's role. If you set it to `"administrator"`, Akismet will always return `false`.
+   * @param string $value The new role.
+   */
+  public function setRole(string $value) {
+    $this->role = $value;
+  }
+
+  /**
+   * Sets the URL of the author's website.
+   * @param string $value The new website URL.
+   */
+  public function setURL(string $value) {
+    $this->url = $value;
+  }
+
+  /**
+   * Sets the author's user agent, that is the string identifying the Web browser used to submit comments.
+   * @param string $value The new user agent.
+   */
+  public function setUserAgent(string $value) {
+    $this->userAgent = $value;
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return array The map in JSON format corresponding to this object.
+   */
+  public function toJSON(): array {
     $map = [];
 
-    if (mb_strlen($this->name)) $map['comment_author'] = $this->name;
-    if (mb_strlen($this->email)) $map['comment_author_email'] = $this->email;
-    if (mb_strlen($this->url)) $map['comment_author_url'] = $this->url;
-    if (mb_strlen($this->userAgent)) $map['user_agent'] = $this->userAgent;
-    if (mb_strlen($this->ipAddress)) $map['user_ip'] = $this->ipAddress;
-    if (mb_strlen($this->role)) $map['user_role'] = $this->role;
+    if (mb_strlen($name = $this->name)) $map['comment_author'] = $name;
+    if (mb_strlen($email = $this->email)) $map['comment_author_email'] = $email;
+    if (mb_strlen($url = $this->url)) $map['comment_author_url'] = $url;
+    if (mb_strlen($userAgent = $this->userAgent)) $map['user_agent'] = $userAgent;
+    if (mb_strlen($ipAddress = $this->ipAddress)) $map['user_ip'] = $ipAddress;
+    if (mb_strlen($role = $this->role)) $map['user_role'] = $role;
 
     return $map;
   }
--- lib/Blog.php
+++ lib/Blog.php
@@ -10,13 +10,110 @@ namespace akismet;
 class Blog implements \JsonSerializable {
 
   /**
+   * @var string The character encoding for the values included in comments.
+   */
+  private $charset = '';
+
+  /**
+   * @var string The language(s) in use on the blog or site, in ISO 639-1 format, comma-separated.
+   */
+  private $language = '';
+
+  /**
+   * @var string The blog or site URL.
+   */
+  private $url = '';
+
+  /**
    * Initializes a new instance of the class.
    * @param array $config Name-value pairs that will be used to initialize the object properties.
    */
-  public function __construct($config = []) {
+  public function __construct(array $config = []) {
     foreach ($config as $property => $value) {
-      if(property_exists($this, $property)) $this->$property = $value;
+      $setter = "set$property";
+      if(method_exists($this, $setter)) $this->$setter($value);
     }
+  }
+
+  /**
+   * Creates a new blog from the specified JSON map.
+   * @param mixed $map A JSON map representing a blog.
+   * @return Blog The instance corresponding to the specified JSON map, or `null` if a parsing error occurred.
+   */
+  public static function fromJSON($map) {
+    return !is_array($map) ? null : new static([
+      'charset' => isset($map['blog_charset']) && is_string($map['blog_charset']) ? $map['blog_charset'] : '',
+      'language' => isset($map['blog_lang']) && is_string($map['blog_lang']) ? $map['blog_lang'] : '',
+      'url' => isset($map['blog']) && is_string($map['blog']) ? $map['blog'] : ''
+    ]);
+  }
+
+  /**
+   * Gets the character encoding for the values included in comments.
+   * @return string The character encoding for the values included in comments.
+   */
+  public function getCharset(): string {
+    return $this->charset;
+  }
+
+  /**
+   * Gets the language(s) in use on the blog or site, in ISO 639-1 format, comma-separated.
+   * @return string The language(s) in use on the blog or site.
+   */
+  public function getLanguage(): string {
+    return $this->language;
+  }
+
+  /**
+   * Gets the blog or site URL.
+   * @return string The blog or site URL.
+   */
+  public function getURL(): string {
+    return $this->url;
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return array The map in JSON format corresponding to this object.
+   */
+  final public function jsonSerialize(): array {
+    return $this->toJSON();
+  }
+
+  /**
+   * Sets the character encoding for the values included in comments.
+   * @param string $value The new character encoding.
+   */
+  public function setCharset(string $value) {
+    $this->charset = $value;
+  }
+
+  /**
+   * Sets the language(s) in use on the blog or site, in ISO 639-1 format, comma-separated.
+   * @param string $value The new language(s).
+   */
+  public function setLanguage(string $value) {
+    $this->language = $value;
+  }
+
+  /**
+   * Sets the blog or site URL.
+   * @param string $value The new URL.
+   */
+  public function setURL(string $value) {
+    $this->url = $value;
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return array The map in JSON format corresponding to this object.
+   */
+  public function toJSON(): array {
+    $map = [];
+    if (mb_strlen($url = $this->getURL())) $map['blog'] = $url;
+    if (mb_strlen($charset = $this->getCharset())) $map['blog_charset'] = $charset;
+    if (mb_strlen($language = $this->getLanguage())) $map['blog_lang'] = $language;
+    return $map;
   }
 
   /**

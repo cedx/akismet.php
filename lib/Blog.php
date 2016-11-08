ce695288049bf5709ee0fd99d046274b57e48081
Cédric Belin <cedric@belin.io>
2016-11-08 15:27:33 +0100

    Implemented some `Blog` and `Comment` methods

--- lib/Blog.php
+++ lib/Blog.php
@@ -7,6 +7,24 @@ namespace akismet;
 /**
  * Represents the front page or home URL transmitted when making requests.
  */
-class Blog {
+class Blog implements \JsonSerializable {
 
+  /**
+   * Initializes a new instance of the class.
+   * @param array $config Name-value pairs that will be used to initialize the object properties.
+   */
+  public function __construct($config = []) {
+    foreach ($config as $property => $value) {
+      if(property_exists($this, $property)) $this->$property = $value;
+    }
+  }
+
+  /**
+   * Returns a string representation of this object.
+   * @return string The string representation of this object.
+   */
+  public function __toString(): string {
+    $json = json_encode($this, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    return static::class . " $json";
+  }
 }
--- lib/Comment.php
+++ lib/Comment.php
@@ -7,6 +7,24 @@ namespace akismet;
 /**
  * Represents a comment submitted by an author.
  */
-class Comment {
+class Comment implements \JsonSerializable {
 
+  /**
+   * Initializes a new instance of the class.
+   * @param array $config Name-value pairs that will be used to initialize the object properties.
+   */
+  public function __construct($config = []) {
+    foreach ($config as $property => $value) {
+      if(property_exists($this, $property)) $this->$property = $value;
+    }
+  }
+
+  /**
+   * Returns a string representation of this object.
+   * @return string The string representation of this object.
+   */
+  public function __toString(): string {
+    $json = json_encode($this, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    return static::class . " $json";
+  }
 }

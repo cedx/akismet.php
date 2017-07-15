9e1cc869a9da430f39afbbbe3291554fec1b7012
Cédric Belin <cedric@belin.io>
2017-07-16 00:51:56 +0200

    Ported the `Client` class to `Observable`

--- lib/Client.php
+++ lib/Client.php
@@ -1,15 +1,17 @@
 <?php
 namespace akismet;
 
-use Evenement\{EventEmitterTrait};
 use GuzzleHttp\{Client as HTTPClient};
+use GuzzleHttp\Promise\{PromiseInterface};
 use GuzzleHttp\Psr7\{ServerRequest};
+use Psr\Http\Message\{ResponseInterface};
+use Rx\{Observable, ObserverInterface};
+use Rx\Subject\{Subject};
 
 /**
  * Submits comments to the [Akismet](https://akismet.com) service.
  */
 class Client implements \JsonSerializable {
-  use EventEmitterTrait;
 
   /**
    * @var string The HTTP header containing the Akismet error messages.
@@ -47,6 +49,16 @@ class Client implements \JsonSerializable {
   private $isTest = false;
 
   /**
+   * @var Subject The handler of "request" events.
+   */
+  private $onRequest;
+
+  /**
+   * @var Subject The handler of "response" events.
+   */
+  private $onResponse;
+
+  /**
    * @var string The user agent string to use when making requests.
    */
   private $userAgent;
@@ -57,6 +69,9 @@ class Client implements \JsonSerializable {
    * @param Blog|string $blog The front page or home URL of the instance making requests.
    */
   public function __construct(string $apiKey = '', $blog = null) {
+    $this->onRequest = new Subject();
+    $this->onResponse = new Subject();
+
     $this->setAPIKey($apiKey);
     $this->setBlog($blog);
     $this->setUserAgent(sprintf('PHP/%s | Akismet/%s', preg_replace('/^(\d+(\.\d+){2}).*/', '$1', PHP_VERSION), static::VERSION));
@@ -74,12 +89,14 @@ class Client implements \JsonSerializable {
   /**
    * Checks the specified comment against the service database, and returns a value indicating whether it is spam.
    * @param Comment $comment The comment to be checked.
-   * @return bool A boolean value indicating whether it is spam.
+   * @return Observable A boolean value indicating whether it is spam.
    */
-  public function checkComment(Comment $comment): bool {
+  public function checkComment(Comment $comment): Observable {
     $serviceURL = parse_url($this->getEndPoint());
-    $endPoint = sprintf('%s://%s.%s/1.1/comment-check', $serviceURL['scheme'], $this->getAPIKey(), $serviceURL['host']);
-    return $this->fetch($endPoint, get_object_vars($comment->jsonSerialize())) == 'true';
+    $endPoint = "{$serviceURL['scheme']}://{$this->getAPIKey()}.{$serviceURL['host']}/1.1/comment-check";
+    return $this->fetch($endPoint, get_object_vars($comment->jsonSerialize()))->map(function(string $response) {
+      return $response == 'true';
+    });
   }
 
   /**
@@ -134,6 +151,22 @@ class Client implements \JsonSerializable {
       'isTest' => $this->isTest(),
       'userAgent' => $this->getUserAgent()
     ];
+  }
+
+  /**
+   * Gets the stream of "request" events.
+   * @return Observable The stream of "request" events.
+   */
+  public function onRequest(): Observable {
+    return $this->onRequest->asObservable();
+  }
+
+  /**
+   * Gets the stream of "response" events.
+   * @return Observable The stream of "response" events.
+   */
+  public function onResponse(): Observable {
+    return $this->onResponse->asObservable();
   }
 
   /**
@@ -194,67 +227,81 @@ class Client implements \JsonSerializable {
   /**
    * Submits the specified comment that was incorrectly marked as spam but should not have been.
    * @param Comment $comment The comment to be submitted.
+   * @return Observable Completes once the comment has been submitted.
    */
-  public function submitHam(Comment $comment) {
+  public function submitHam(Comment $comment): Observable {
     $serviceURL = parse_url($this->getEndPoint());
-    $endPoint = sprintf('%s://%s.%s/1.1/submit-ham', $serviceURL['scheme'], $this->getAPIKey(), $serviceURL['host']);
-    $this->fetch($endPoint, get_object_vars($comment->jsonSerialize()));
+    $endPoint = "{$serviceURL['scheme']}://{$this->getAPIKey()}.{$serviceURL['host']}/1.1/submit-ham";
+    return $this->fetch($endPoint, get_object_vars($comment->jsonSerialize()));
   }
 
   /**
    * Submits the specified comment that was not marked as spam but should have been.
    * @param Comment $comment The comment to be submitted.
+   * @return Observable Completes once the comment has been submitted.
    */
-  public function submitSpam(Comment $comment) {
+  public function submitSpam(Comment $comment): Observable {
     $serviceURL = parse_url($this->getEndPoint());
-    $endPoint = sprintf('%s://%s.%s/1.1/submit-spam', $serviceURL['scheme'], $this->getAPIKey(), $serviceURL['host']);
-    $this->fetch($endPoint, get_object_vars($comment->jsonSerialize()));
+    $endPoint = "{$serviceURL['scheme']}://{$this->getAPIKey()}.{$serviceURL['host']}/1.1/submit-spam";
+    return $this->fetch($endPoint, get_object_vars($comment->jsonSerialize()));
   }
 
   /**
    * Checks the API key against the service database, and returns a value indicating whether it is valid.
-   * @return bool A boolean value indicating whether it is a valid API key.
+   * @return Observable A boolean value indicating whether it is a valid API key.
    */
-  public function verifyKey(): bool {
+  public function verifyKey(): Observable {
     $endPoint = $this->getEndPoint().'/1.1/verify-key';
-    return $this->fetch($endPoint, ['key' => $this->getAPIKey()]) == 'valid';
+    return $this->fetch($endPoint, ['key' => $this->getAPIKey()])->map(function(string $response) {
+      return $response == 'valid';
+    });
   }
 
   /**
    * Queries the service by posting the specified fields to a given end point, and returns the response as a string.
    * @param string $endPoint The URL of the end point to query.
    * @param array $fields The fields describing the query body.
-   * @return string The response body.
+   * @return Observable The response body as string.
    * @emits \GuzzleHttp\Psr7\ServerRequest The "request" event.
    * @emits \GuzzleHttp\Psr7\Response The "response" event.
-   * @throws \InvalidArgumentException The API key or the blog URL is empty.
-   * @throws \RuntimeException An error occurred while querying the end point.
    */
-  private function fetch(string $endPoint, array $fields = []): string {
+  private function fetch(string $endPoint, array $fields = []): Observable {
     $blog = $this->getBlog();
-    if (!mb_strlen($this->getAPIKey()) || !$blog) throw new \InvalidArgumentException('The API key or the blog URL is empty.');
+    if (!mb_strlen($this->getAPIKey()) || !$blog)
+      return Observable::error(new \InvalidArgumentException('The API key or the blog URL is empty.'));
 
     $bodyFields = array_merge(get_object_vars($blog->jsonSerialize()), $fields);
     if ($this->isTest()) $bodyFields['is_test'] = '1';
 
-    try {
-      $request = (new ServerRequest('POST', $endPoint))->withParsedBody($bodyFields);
-      $this->emit('request', [$request]);
+    $request = (new ServerRequest('POST', $endPoint))->withParsedBody($bodyFields);
+    $promise = (new HTTPClient)->sendAsync($request, [
+      'form_params' => $request->getParsedBody(),
+      'headers' => ['User-Agent' => $this->getUserAgent()]
+    ]);
 
-      $response = (new HTTPClient)->send($request, [
-        'form_params' => $request->getParsedBody(),
-        'headers' => ['User-Agent' => $this->getUserAgent()]
-      ]);
-
-      $this->emit('reponse', [$response]);
-      if($response->hasHeader(static::DEBUG_HEADER))
-        throw new \UnexpectedValueException($response->getHeader(static::DEBUG_HEADER)[0]);
-
+    $this->onRequest->onNext($request);
+    return static::toObservable($promise)->map(function(ResponseInterface $response) {
+      $this->onResponse->onNext($response);
+      if($response->hasHeader(static::DEBUG_HEADER)) throw new \UnexpectedValueException($response->getHeader(static::DEBUG_HEADER)[0]);
       return (string) $response->getBody();
-    }
+    });
+  }
 
-    catch (\Throwable $e) {
-      throw new \RuntimeException('An error occurred while querying the end point.');
-    }
+  /**
+   * Converts a `Promise` to an `Observable`.
+   * @param PromiseInterface $promise The `Promise` to be converted.
+   * @return Observable An `Observable` which wraps the `Promise`.
+   */
+  private static function toObservable(PromiseInterface $promise): Observable {
+    return Observable::create(function(ObserverInterface $observer) use ($promise) {
+      try {
+        $observer->onNext($promise->wait());
+        $observer->onCompleted();
+      }
+
+      catch (\Throwable $e) {
+        $observer->onError($e);
+      }
+    });
   }
 }

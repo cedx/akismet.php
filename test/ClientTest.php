081794cfeca82964ebe2d605ce08302e5bc11102
Cédric Belin <cedric@belin.io>
2018-10-18 13:36:47 +0200

    Updated the documentation

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -83,7 +83,7 @@ class ClientTest extends TestCase {
   }
 
   /**
-   * Performs a common set of tasks just before each test method is called.
+   * This method is called before each test.
    * @before
    */
   protected function setUp(): void {

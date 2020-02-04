0f9962f61335b93848d6dad5b0fd34fccdf47490
Cédric Belin <cedric@belin.io>
2020-02-04 21:30:12 +0100

    Updated the user agent string

--- test/Http/ClientTest.php
+++ test/Http/ClientTest.php
@@ -59,7 +59,7 @@ class ClientTest extends TestCase {
   protected function setUp(): void {
     $this->client = (new Client((string) getenv('AKISMET_API_KEY'), new Blog(new Uri('https://dev.belin.io/akismet.php'))))->setTest(true);
 
-    $author = (new Author('192.168.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0', 'Akismet'))
+    $author = (new Author('192.168.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71', 'Akismet'))
       ->setRole('administrator')
       ->setUrl(new Uri('https://dev.belin.io/akismet.php'));
 

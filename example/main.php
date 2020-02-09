5ae45ed7db494ef3f1ba17d87c9d281200b26603
Cédric Belin <cedric@belin.io>
2020-02-09 15:19:18 +0100

    Updated the example code

--- example/main.php
+++ example/main.php
@@ -1,26 +1,31 @@
 <?php declare(strict_types=1);
 
-use Akismet\{Author, Blog, Comment, CommentType};
+use Akismet\{Author, Blog, CheckResult, Comment, CommentType};
 use Akismet\Http\{Client, ClientException};
 use GuzzleHttp\Psr7\{Uri};
 
 /** Queries the Akismet service. */
 function main(): void {
   try {
+    $blog = new Blog(new Uri('https://www.yourblog.com'), 'UTF-8', ['fr']);
+    $client = new Client('123YourAPIKey', $blog);
+
     // Key verification.
-    $client = new Client('123YourAPIKey', new Blog(new Uri('https://www.yourblog.com')));
     $isValid = $client->verifyKey();
     echo $isValid ? 'The API key is valid' : 'The API key is invalid';
 
     // Comment check.
-    $comment = new Comment(
-      new Author('127.0.0.1', 'Mozilla/5.0'),
-      'A user comment',
-      CommentType::contactForm
-    );
+    $author = (new Author(
+      '192.168.123.456',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+      'John Doe'
+    ))->setEmail('john.doe@domain.com')->setRole('guest');
 
-    $isSpam = $client->checkComment($comment);
-    echo $isSpam ? 'The comment is spam' : 'The comment is ham';
+    $comment = (new Comment($author, 'A user comment', CommentType::contactForm))
+      ->setDate(new \DateTimeImmutable);
+
+    $result = $client->checkComment($comment);
+    echo $result == CheckResult::isHam ? 'The comment is ham' : 'The comment is spam';
 
     // Submit spam / ham.
     $client->submitSpam($comment);

eaad8b9d8634b6ea9e54e3adb210681f44f00bc1
Cédric Belin <cedric@belin.io>
2020-02-09 15:19:35 +0100

    Added the `CheckResult` enumeration

---
+++ lib/CheckResult.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+namespace Akismet;
+
+/** Specifies the result of a comment check. */
+abstract class CheckResult {
+
+  /** @var string The comment is not a spam (i.e. a ham). */
+  const isHam = 'isHam';
+
+  /** @var string The comment is a pervasive spam (i.e. it can be safely discarded). */
+  const isPervasiveSpam = 'isPervasiveSpam';
+
+  /** @var string The comment is a spam. */
+  const isSpam = 'isSpam';
+}

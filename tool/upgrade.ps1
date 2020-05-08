f8c171207e55caf7eea91b7d4318af39cfb1b4c0
Cédric Belin <cedric@belin.io>
2020-05-08 03:18:14 +0200

    Fixed the "upgrade" task

--- tool/upgrade.ps1
+++ tool/upgrade.ps1
@@ -7,4 +7,4 @@ git fetch --all --prune
 git pull --rebase
 
 $composer = $IsWindows ? 'php "C:\Program Files\PHP\share\composer.phar"' : 'composer'
-& $composer update --no-interaction
+Invoke-Expression "$composer update --no-interaction"

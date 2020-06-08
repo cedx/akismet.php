8715463a06ac08b467c95050cceda5beac1cf52c
Cédric Belin <cedric@belin.io>
2020-06-08 17:19:22 +0200

    Removed the "upgrade" script

--- tool/upgrade.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-Set-Location (Split-Path $PSScriptRoot)
-
-git reset --hard
-git fetch --all --prune
-git pull --rebase
-
-composer update --no-interaction

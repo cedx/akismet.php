43885997108cda6280fa64f9c65fdedc1ad8812b
Cédric Belin <cedric@belin.io>
2020-06-08 14:52:19 +0200

    Updated the "doc" task

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -2,9 +2,7 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-$phpdoc = $IsWindows ? 'php "C:/Program Files/PHP/share/phpDocumentor.phar"' : "phpdoc";
-Invoke-Expression "$phpdoc --config=etc/phpdoc.xml"
-
+phpdoc --config=etc/phpdoc.xml
 if (-not (Test-Path doc/api/images)) { New-Item doc/api/images -ItemType Directory | Out-Null }
 Copy-Item doc/img/favicon.ico doc/api/images
 mkdocs build --config-file=etc/mkdocs.yaml

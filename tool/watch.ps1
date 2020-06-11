48d265557e35648f8b8dbe1b67cde9c2150d33b8
Cédric Belin <cedric@belin.io>
2020-06-11 18:17:02 +0200

    Updated the "watch" task

--- tool/watch.ps1
+++ tool/watch.ps1
@@ -7,7 +7,7 @@ $action = {
 	if ($EventArgs.Name -notlike "*.g.php") {
 		$changeType = [String] $EventArgs.ChangeType
 		Write-Host "'$($EventArgs.Name)' was $($changeType.ToLower()): starting a new build..."
-		$timeSpan = Measure-Command { tool/build.ps1 }
+		$timeSpan = Measure-Command { tool/build.ps1 | Out-Host }
 		Write-Host "> Finished the build after $($timeSpan.TotalSeconds) seconds."
 	}
 }

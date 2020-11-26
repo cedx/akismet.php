365df2e1891de041d2d59e7f63628224e6fc7c31
Cédric Belin <cedric@belin.io>
2020-11-26 11:36:56 +0100

    Updated the build system

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -3,6 +3,5 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 phpdoc --config=etc/phpdoc.xml
-if (-not (Test-Path doc/api/images)) { New-Item doc/api/images -ItemType Directory | Out-Null }
-Copy-Item doc/img/favicon.ico doc/api/images
-mkdocs build --config-file=etc/mkdocs.yaml
+if (-not (Test-Path docs/api/images)) { New-Item docs/api/images -ItemType Directory | Out-Null }
+Copy-Item docs/favicon.ico docs/api/images

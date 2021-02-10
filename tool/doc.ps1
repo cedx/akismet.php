169253e0d073d3aee958591c75d04e2388f55734
Cédric Belin <cedric@belin.io>
2021-02-10 20:24:26 +0100

    Updated the "doc" task

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -2,6 +2,8 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
+if (Test-Path docs/api) { Remove-Item docs/api -Force -Recurse }
 phpdoc --config=etc/phpdoc.xml
+
 if (-not (Test-Path docs/api/images)) { New-Item docs/api/images -ItemType Directory | Out-Null }
 Copy-Item docs/favicon.ico docs/api/images

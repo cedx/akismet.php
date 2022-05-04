2362fdd265dabcda0ca0679a1a25d36b685e02c6
Cédric Belin <cedric@belin.io>
2022-05-05 00:58:58 +0200

    Ported the build system to Gulp

--- tool/doc.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-Set-Location (Split-Path $PSScriptRoot)
-
-if (Test-Path docs) { Remove-Item docs -Force -Recurse }
-phpdoc --config=etc/phpdoc.xml
-
-if (-not (Test-Path docs/images)) { New-Item docs/images -ItemType Directory | Out-Null }
-Copy-Item www/favicon.ico docs/images

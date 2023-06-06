506d31385cc2a583cef8890888df339b0e191538
Cédric Belin <cedric@belin.io>
2023-06-06 22:12:06 +0200

    Update the "Doc" task

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -1,13 +1,15 @@
 import haxe.Json;
 import sys.FileSystem;
 import sys.io.File;
+using Lambda;
 
 /** Builds the documentation. **/
 function main() {
+	["CHANGELOG.md", "LICENSE.md"].iter(file -> File.copy(file, 'docs/${file.toLowerCase()}'));
+	if (FileSystem.exists("docs/api")) Tools.removeDirectory("docs/api");
+
 	final version = Json.parse(File.getContent("composer.json")).version;
 	Tools.replaceInFile("etc/phpdoc.xml", ~/version number="\d+(\.\d+){2}"/, 'version number="$version"');
-
-	if (FileSystem.exists("docs/api")) Tools.removeDirectory("docs/api");
 	Sys.command("phpdoc --config=etc/phpdoc.xml");
 	File.copy("docs/favicon.ico", "docs/api/images/favicon.ico");
 }

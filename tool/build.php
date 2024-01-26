98787a4cd070ab9be2f4b16f5c6dbcd5d114532d
Cédric Belin <cedric@belin.io>
2024-01-26 22:14:32 +0100

    Update the documentation [skip ci]

--- tool/build.php
+++ tool/build.php
@@ -1,7 +1,5 @@
 <?php require __DIR__."/tools.php";
 
-/**
- * Updates the version number in the sources.
- */
+// Updates the version number in the sources.
 $version = json_decode((string) file_get_contents(__DIR__."/../composer.json"))->version;
 replaceInFile("src/Client.php", '/const version = "\d+(\.\d+){2}"/', "const version = \"$version\"");

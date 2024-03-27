fffa0630a2a3cbdb492b37102c5769159af51970
Cédric Belin <cedric@belin.io>
2024-03-27 13:00:32 +0100

    Port the build system to Castor

--- tool/build.php
+++
@@ -1,5 +0,0 @@
-<?php require __DIR__."/tools.php";
-
-// Updates the version number in the sources.
-$version = json_decode((string) file_get_contents(__DIR__."/../composer.json"))->version;
-replaceInFile("src/Client.php", '/version = "\d+(\.\d+){2}"/', "version = \"$version\"");

c16e2b24833e4b5795fa3df2941f58e056215c88
Cédric Belin <cedric@belin.io>
2024-04-26 15:28:25 +0200

    Bump the version number

--- src/ClientException.php
+++
@@ -1,8 +0,0 @@
-<?php namespace akismet;
-
-use Psr\Http\Client\ClientExceptionInterface;
-
-/**
- * An exception caused by an error in a {@see Client} request.
- */
-class ClientException extends \RuntimeException implements ClientExceptionInterface {}

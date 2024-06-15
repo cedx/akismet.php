ca66856b1766537c90924610ac780c4a09f051b3
Cédric Belin <cedric@belin.io>
2024-06-15 22:11:03 +0200

    Update the "User-Agent" string [skip ci]

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -75,7 +75,7 @@ final class ClientTest extends TestCase {
 				name: "Akismet",
 				role: AuthorRole::administrator->value,
 				url: "https://belin.io",
-				userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+				userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
 			),
 			content: "I'm testing out the Service API.",
 			referrer: "https://www.npmjs.com/package/@cedx/akismet",

9753a581401c63860ae5d915927d9e6a97acafd7
Cédric Belin <cedx@outlook.com>
2026-01-21 01:22:45 +0100

    Fix the regressions

--- castor.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-// TODO !!!
-#[AsTask(description: "Builds the project")]
-function build(): void {
-	$file = "src/Client.php";
-	$pkg = variable("package");
-	file_put_contents($file, preg_replace('/version = "\d+(\.\d+){2}"/', "version = \"$pkg->version\"", file_get_contents($file)));
-}
--- src/Client.php
+++ src/Client.php
@@ -6,27 +6,27 @@ use Uri\Rfc3986\Uri;
 /**
  * Submits comments to the [Akismet](https://akismet.com) service.
  */
-final readonly class Client {
+final class Client {
 
 	/**
 	 * The response returned by the `submit-ham` and `submit-spam` endpoints when the outcome is a success.
 	 */
-	private const string success = "Thanks for making the web a better place.";
+	private const string Success = "Thanks for making the web a better place.";
 
 	/**
-	 * The package version.
+	 * The package version number.
 	 */
-	private const string version = "16.0.0";
+	private static ?string $version = null;
 
 	/**
 	 * The Akismet API key.
 	 */
-	public string $apiKey;
+	public readonly string $apiKey;
 
 	/**
 	 * The base URL of the remote API endpoint.
 	 */
-	public Uri $baseUrl;
+	public readonly Uri $baseUrl;
 
 	/**
 	 * The front page or home URL of the instance making requests.
@@ -52,11 +52,13 @@ final readonly class Client {
 	 * @param string|Uri $baseUrl The base URL of the remote API endpoint.
 	 */
 	function __construct(string $apiKey, Blog $blog, bool $isTest = false, string $userAgent = "", string|Uri $baseUrl = "https://rest.akismet.com") {
+		self::$version ??= json_decode((string) file_get_contents(__DIR__ . "/../composer.json"))->version;
+
 		$this->apiKey = $apiKey;
 		$this->baseUrl = new Uri(mb_rtrim($baseUrl instanceof Uri ? $baseUrl->toString() : $baseUrl, "/"));
 		$this->blog = $blog;
 		$this->isTest = $isTest;
-		$this->userAgent = $userAgent ?: sprintf("PHP/%d | Akismet/%s", PHP_MAJOR_VERSION, self::version);
+		$this->userAgent = $userAgent ?: sprintf("PHP/%d | Akismet/%s", PHP_MAJOR_VERSION, self::$version);
 	}
 
 	/**
@@ -67,9 +69,8 @@ final readonly class Client {
 	 */
 	function checkComment(Comment $comment): CheckResult {
 		$response = $this->fetch("1.1/comment-check", $comment->jsonSerialize());
-		return (string) $response->getBody() == "false"
-			? CheckResult::Ham
-			: ($response->getHeaderLine("x-akismet-pro-tip") == "discard" ? CheckResult::PervasiveSpam : CheckResult::Spam);
+		if ($response->body == "false") return CheckResult::Ham;
+		return ($response->headers["x-akismet-pro-tip"] ?? "") == "discard" ? CheckResult::PervasiveSpam : CheckResult::Spam;
 	}
 
 	/**
@@ -79,7 +80,7 @@ final readonly class Client {
 	 */
 	function submitHam(Comment $comment): void {
 		$response = $this->fetch("1.1/submit-ham", $comment->jsonSerialize());
-		if ((string) $response->getBody() != self::success) throw new \RuntimeException("Invalid server response.", 500);
+		if ($response->body != self::Success) throw new \RuntimeException("Invalid server response.", 500);
 	}
 
 	/**
@@ -89,7 +90,7 @@ final readonly class Client {
 	 */
 	function submitSpam(Comment $comment): void {
 		$response = $this->fetch("1.1/submit-spam", $comment->jsonSerialize());
-		if ((string) $response->getBody() != self::success) throw new \RuntimeException("Invalid server response.", 500);
+		if ($response->body != self::Success) throw new \RuntimeException("Invalid server response.", 500);
 	}
 
 	/**
@@ -98,8 +99,8 @@ final readonly class Client {
 	 */
 	function verifyKey(): bool {
 		try {
-			$response = $this->fetch("1.1/verify-key", new \stdClass);
-			return (string) $response->getBody() == "valid";
+			$response = $this->fetch("1.1/verify-key");
+			return $response->body == "valid";
 		}
 		catch (\RuntimeException) {
 			return false;
@@ -110,15 +111,15 @@ final readonly class Client {
 	 * Queries the service by posting the specified fields to a given end point, and returns the response.
 	 * @param string $endpoint The URL of the end point to query.
 	 * @param object $fields The fields describing the query body.
-	 * @return Response The server response.
+	 * @return \stdClass The server response.
 	 * @throws \RuntimeException An error occurred while querying the end point.
 	 */
-	private function fetch(string $endpoint, object $fields): Response {
+	private function fetch(string $endpoint, ?object $fields = null): \stdClass {
 		$handle = curl_init($this->baseUrl->withPath("{$this->baseUrl->getPath()}/$endpoint")->toString());
 		if (!$handle) throw new \RuntimeException("Unable to allocate the cURL handle.", 500);
 
 		$postFields = $this->blog->jsonSerialize();
-		foreach (get_object_vars($fields) as $key => $value) $postFields->$key = $value;
+		foreach (get_object_vars($fields ?? new \stdClass) as $key => $value) $postFields->$key = $value;
 		$postFields->api_key = $this->apiKey;
 		if ($this->isTest) $postFields->is_test = "1";
 
@@ -126,7 +127,7 @@ final readonly class Client {
 		curl_setopt_array($handle, [
 			CURLOPT_FOLLOWLOCATION => true,
 			CURLOPT_HEADERFUNCTION => function($_, $header) use (&$headers) {
-				if (count($parts = explode(":", $header, 2)) == 2) $headers[mb_trim($parts[0])][] = mb_trim($parts[1]);
+				if (count($parts = explode(":", $header, 2)) == 2) $headers[mb_trim($parts[0]) |> mb_strtolower(...)] = mb_trim($parts[1]);
 				return strlen($header);
 			},
 			CURLOPT_POST => true,
@@ -138,11 +139,10 @@ final readonly class Client {
 		$body = curl_exec($handle);
 		if ($body === false) throw new \RuntimeException(curl_error($handle), 500);
 
-		$response = new Response(curl_getinfo($handle, CURLINFO_RESPONSE_CODE), $headers, (string) $body);
-		if (intdiv($status = $response->getStatusCode(), 100) != 2) throw new \RuntimeException($response->getReasonPhrase(), $status);
-
-		return $response->hasHeader("x-akismet-alert-code")
-			? throw new \RuntimeException("{$response->getHeaderLine("x-akismet-alert-code")} {$response->getHeaderLine("x-akismet-alert-msg")}", 403)
-			: ($response->hasHeader("x-akismet-debug-help") ? throw new \RuntimeException($response->getHeaderLine("x-akismet-debug-help"), 400) : $response);
+		$statusCode = curl_getinfo($handle, CURLINFO_RESPONSE_CODE);
+		if (intdiv($statusCode, 100) != 2) throw new \RuntimeException("The server response failed.", $statusCode);
+		if (isset($headers["x-akismet-alert-code"])) throw new \RuntimeException($headers["x-akismet-alert-msg"], 403);
+		if (isset($headers["x-akismet-debug-help"])) throw new \RuntimeException($headers["x-akismet-debug-help"], 400);
+		return (object) ["headers" => $headers, "body" => (string) $body];
 	}
 }

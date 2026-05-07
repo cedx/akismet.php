69cb2ccd734cf5eadcc714fe524f91f03dd24887
Cédric Belin <cedx@outlook.com>
2026-05-07 22:17:42 +0200

    Code optimization [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -3,4 +3,4 @@ using module ./Cmdlets.psm1
 
 "Performing the static analysis of source code..."
 Invoke-ScriptAnalyzer $PSScriptRoot -ExcludeRule PSUseShouldProcessForStateChangingFunctions -Recurse
-Invoke-PhpStan "$PSScriptRoot/../etc/PHPStan.php" -MemoryLimit 256mb
+Invoke-PhpStan etc/PHPStan.php -MemoryLimit 256mb
--- tool/Test.ps1
+++ tool/Test.ps1
@@ -1,4 +1,4 @@
 using module ./Cmdlets.psm1
 
 "Running the test suite..."
-Invoke-PhpUnit "$PSScriptRoot/../etc/PHPUnit.xml"
+Invoke-PhpUnit etc/PHPUnit.xml
